1e83004c7263b68b5f55d6b8706ebfefcbd3d7e0
lochoe <shahrul.id@gmail.com>
2024-08-31 10:57:41 +0800

    Remove config.js file containing sensitive information

--- config.js
+++
@@ -1,5 +0,0 @@
-var FOLDER_ID = '1ziIy03_asT8_C3X-Cs9QiFqzCgiBndsk'; // Replace with your actual folder ID
-var SPREADSHEET_ID = '1yYURBm6p6xjVypXMM5kZCWSM47J4fSudAeCRrpWCnGg'; // Replace with your actual spreadsheet ID
-var SHEET_NAME = 'pawnShop'; // Replace with your actual sheet name
-var VISION_API_KEY = 'AIzaSyDhLGkCR4IZnM6fGhxblYpzS5WNFZFr8tU'; // Replace with your Google Cloud Vision API key
-
